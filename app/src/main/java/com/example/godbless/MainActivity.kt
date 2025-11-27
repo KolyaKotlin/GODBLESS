b2db23720a9b34c78b3af24c79b255e44923e448
KolyaKotlin <ardyhdxixhs223455@gmail.com>
2025-11-27 21:46:16 +0300

    пздц

--- app/src/main/java/com/example/godbless/MainActivity.kt
+++ app/src/main/java/com/example/godbless/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
